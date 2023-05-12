94c8abf172e3f701dd90de8c9f394f2be8f38f3a
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-05-12 14:20:00 +0900

    add dp_f, dp_g answer

--- dp/f/main.py
+++ dp/f/main.py
@@ -1,5 +1,37 @@
+from collections import defaultdict
+
 def main():
-    pass
+    s = input()
+    t = input()
+
+    dp = defaultdict(lambda: defaultdict(int))
+    for i in range(1, len(s) + 1):
+        s_idx = i - 1
+        for j in range(1, len(t) + 1):
+            t_idx = j - 1
+
+            if s[s_idx] == t[t_idx]:
+                dp[i][j] = dp[i - 1][j - 1] + 1
+            else:
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+    ans = []
+    i = len(s)
+    j = len(t)
+
+    while len(ans) < dp[len(s)][len(t)]:
+        if s[i - 1] == t[j - 1]:
+            ans.insert(0, s[i - 1])
+            i -= 1
+            j -= 1
+        else:
+            if dp[i][j] == dp[i - 1][j]:
+                i -= 1
+            else: 
+                j -= 1
+
+    print("".join(ans))
+
 
 
 if __name__ == "__main__":
--- dp/g/main.py
+++ dp/g/main.py
@@ -1,5 +1,38 @@
+from collections import defaultdict
+import sys
+
+sys.setrecursionlimit(10 ** 8)
+
 def main():
-    pass
+    N, M = map(int, input().split())
+    
+    gragh = defaultdict(set)
+    for _ in range(M):
+        x, y = map(int, input().split())
+        gragh[x].add(y)
+
+    flg = [False for _ in range(N + 1)]
+    dp = [0 for _ in range(N + 1)]
+    def rec(x):
+        if flg[x]:
+            return dp[x]
+        
+        m = 0
+        for v in gragh[x]:
+            m = max(m, rec(v) + 1)
+
+        dp[x] = m
+        flg[x] = True
+        return m
+
+    ans = 0
+    for n in range(1, N + 1):
+        ans = max(ans, rec(n))    
+
+    print(ans)
+        
+
+    
 
 
 if __name__ == "__main__":
