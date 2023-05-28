44a2ef3252f44f8c2bdbd7192098fb2b417dc5ef
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-05-28 17:50:20 +0900

    abc277_d

--- abc/abc277/d/main.py
+++ abc/abc277/d/main.py
@@ -1,41 +1,42 @@
-from collections import defaultdict
-
 def main():
-    with open(r"C:\Users\iwbc_\PycharmProjects\Sandbox\atcoder\abc277\d\tests\005.in", "r") as f:
-        N, M = map(int, f.readline().split())
-        A = list(map(int, f.readline().split()))
-
-    # N, M = map(int, input().split())
-    # A = list(map(int, input().split()))
-
-    d = defaultdict(int)
-    a_sum = 0
-    keys = set()
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    
+    count = {}
+    total = 0
     for a in A:
-        d[a] += a
-        a_sum += a
-        keys.add(a)
+        total += a
+        if a not in count:
+            count[a] = 0
+        count[a] += 1
 
-    ans = a_sum
-    cache = defaultdict(int)
-    # keys = set(d.keys())
-    for start in keys:
-        s = 0
+    if len(count.keys()) == M:
+        print(0)
+        return
+
+    ans = total
+    visited = set()
+    for start in count.keys():
+        if start in visited:
+            continue
+
+        prev = (start - 1) % M
+        if prev not in visited and prev in count:
+            continue
+
         next_key = start
-        while d[next_key]:
-        # while next_key in keys:
-            if cache[next_key]:
-                s += cache[next_key]
-                break
+        visited.add(start)
+        m = 0
+        while next_key >= 0:
+            m += count[next_key] * next_key
 
-            s += d[next_key]
             next_key = (next_key + 1) % M
-
-        cache[start] = s
-        ans = min(ans, a_sum - s)
-        if ans == 0:
-            pass
-
+            if next_key not in count or next_key in visited:
+                next_key = -1
+                break
+        
+        ans = min(ans, total - m)
+                
     print(ans)
 
 
