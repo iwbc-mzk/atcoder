d2a45a44e468ce2b61f4bd913e172711ad725e29
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-06-07 20:09:19 +0900

    abc172_d

--- abc/abc172/d/main.py
+++ abc/abc172/d/main.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+    N = int(input())
+
+    ans = 0
+    for n in range(1, N + 1):
+        i = N // n
+        s = i * (2 * n + (i - 1) * n) // 2
+        ans += s
+
+    print(ans)
 
 
 if __name__ == "__main__":
