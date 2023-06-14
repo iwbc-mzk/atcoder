e46bc9138abf92b03c7fc04bed762b5ceff25ef6
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-06-14 12:15:41 +0900

    ヘルパー関数の修正

--- .helper/rotate.py
+++ .helper/rotate.py
@@ -1,11 +1,9 @@
 import math
 
 
-def rotate(
-    radian: float, x: float, y: float, cx: float = 0.0, cy: float = 0.0
-) -> tuple[float, float]:
-    cosz = math.cos(math.radians(radian))
-    sinz = math.sin(math.radians(radian))
+def rotate(angle_degree: float, x: float, y: float, cx: float = 0.0, cy: float = 0.0):
+    cosz = math.cos(math.radians(angle_degree))
+    sinz = math.sin(math.radians(angle_degree))
 
     xx = (x - cx) * cosz - (y - cy) * sinz + cx
     yy = (y - cy) * cosz + (x - cx) * sinz + cy
