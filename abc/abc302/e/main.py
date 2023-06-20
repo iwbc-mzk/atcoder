e88115b743a4b689e6d4a8de690f970a91f2bf66
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-06-20 15:34:55 +0900

    abc302_e

--- abc/abc302/e/main.py
+++ abc/abc302/e/main.py
@@ -1,14 +1,31 @@
 from collections import defaultdict
 
+
 def main():
     N, Q = map(int, input().split())
     gragh = defaultdict(set)
+    ans = N
     for q in range(Q):
-        query = list(map(int, input().split()))
-        if query[0] == 1:
-            u, v = query[1:]
+        n, u, *v = list(map(int, input().split()))
+        if n == 1:
+            v = v[0]
+            if not gragh[u]:
+                ans -= 1
+            if not gragh[v]:
+                ans -= 1
+
             gragh[u].add(v)
             gragh[v].add(u)
+        else:
+            for vv in gragh[u]:
+                gragh[vv].remove(u)
+                if not gragh[vv]:
+                    ans += 1
+
+            ans += 1 if gragh[u] else 0
+            gragh[u] = set()
+
+        print(ans)
 
 
 if __name__ == "__main__":
