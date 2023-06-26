39d02e31f039c56667cb5d3ecd8247f7666f307a
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-06-26 15:04:23 +0900

    arc162_b

--- arc/arc162/b/main.py
+++ arc/arc162/b/main.py
@@ -1,39 +1,51 @@
+def swap(indexes, i, j):
+    i -= 1
+    num, next_num = None, None
+    for k in indexes:
+        if indexes[k] == i:
+            num = k
+
+        if indexes[k] == i + 1:
+            next_num = k
+
+        if j <= indexes[k] < i:
+            indexes[k] += 2
+
+    indexes[num] = j
+    indexes[next_num] = j + 1
+
+
 def main():
     N = int(input())
     P = list(map(int, input().split()))
 
-    idxs = {}
-    for i in range(N):
-        idxs[P[i]] = i
-
+    indexes = {}
     ans = []
-    for num in range(1, N):
-        i = idxs[num]
-        if i == N - 1:
-            break
+    for i in range(N):
+        indexes[P[i]] = i
 
-        j = num - 1
-        if i == j:
+    for i in range(N - 1):
+        num = i + 1
+        if indexes[num] == i:
             continue
+        else:
+            num_idx = indexes[num]
+            if num_idx == N - 1:
+                swap(indexes, N - 1, N - 3)
+                ans.append((N - 1, N - 3))
+                num_idx = indexes[num]
+            swap(indexes, num_idx + 1, i)
+            ans.append((num_idx + 1, i))
 
-        ans.append((i, j))
-        for key in idxs:
-            if idxs[key] == i:
-                idxs[key] = j
-            elif idxs[key] == i + 1:
-                idxs[key] = j + 1
-            elif j <= idxs[key] <= i:
-                idxs[key] += 2
-
-    for k, v in idxs.items():
+    for k, v in indexes.items():
         if k != v + 1:
             print("No")
             return
-    else:
-        print("Yes")
-        print(len(ans))
-        for i, j in ans:
-            print(i + 1, j)
+
+    print("Yes")
+    print(len(ans))
+    for a in ans:
+        print(*a)
 
 
 if __name__ == "__main__":
