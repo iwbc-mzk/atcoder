5bad2cbcaa90f79fc786003c70e46232f636e6f9
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-12 20:49:48 +0900

    typical90_002

--- typical90/002/main.py
+++ typical90/002/main.py
@@ -1,5 +1,33 @@
+import sys
+
+sys.setrecursionlimit(10**9)
+
+
 def main():
-    pass
+    N = int(input())
+
+    if N % 2:
+        return
+
+    l = []
+
+    def rec(s, c):
+        if len(s) == N:
+            if c == 0:
+                l.append(s)
+            return
+
+        if c == 0:
+            s += "("
+            rec(s, c + 1)
+        else:
+            rec(s + "(", c + 1)
+            rec(s + ")", c - 1)
+
+    rec("", 0)
+    l.sort()
+    for li in l:
+        print(li)
 
 
 if __name__ == "__main__":
