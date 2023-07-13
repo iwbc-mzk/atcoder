088b3c6fee4460ff9bdccc21862890f2391721a2
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-13 17:09:27 +0900

    typical90_004

--- typical90/004/main.py
+++ typical90/004/main.py
@@ -1,5 +1,17 @@
 def main():
-    pass
+    H, W = map(int, input().split())
+
+    A = [list(map(int, input().split())) for _ in range(H)]
+    sum_h = list(map(sum, A))
+    sum_w = list(map(sum, zip(*A)))
+
+    for i in range(H):
+        ans = []
+        for j in range(W):
+            v = sum_h[i] + sum_w[j] - A[i][j]
+            ans.append(v)
+
+        print(*ans)
 
 
 if __name__ == "__main__":
