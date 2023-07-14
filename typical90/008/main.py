f7ae9d2235a7a4db2bf28ead5f050544306c55eb
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-14 11:05:23 +0900

    typical90_008

--- typical90/008/main.py
+++ typical90/008/main.py
@@ -1,5 +1,20 @@
 def main():
-    pass
+    MOD = 10**9 + 7
+
+    N = int(input())
+    S = input()
+
+    DP = [[0 for _ in range(len("atcoder") + 1)] for _ in range(N + 1)]
+    DP[0][0] = 1
+    for i, s in enumerate(S, 1):
+        for j, si in enumerate(" atcoder"):
+            if s == si:
+                DP[i][j] += DP[i - 1][j - 1] % MOD
+
+            DP[i][j] += DP[i - 1][j] % MOD
+            DP[i][j] = DP[i][j] % MOD
+
+    print(DP[N][7])
 
 
 if __name__ == "__main__":
