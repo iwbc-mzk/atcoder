a4177cb34d7be61b14187f1fd4d71fa4a6bf0a22
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-14 11:10:46 +0900

    typical90_010

--- typical90/010/main.py
+++ typical90/010/main.py
@@ -1,5 +1,21 @@
 def main():
-    pass
+    N = int(input())
+
+    C1 = [0]
+    C2 = [0]
+    for n in range(N):
+        C, P = map(int, input().split())
+        if C == 1:
+            C1.append(C1[n] + P)
+            C2.append(C2[n])
+        else:
+            C1.append(C1[n])
+            C2.append(C2[n] + P)
+
+    Q = int(input())
+    for _ in range(Q):
+        L, R = map(int, input().split())
+        print(C1[R] - C1[L - 1], C2[R] - C2[L - 1])
 
 
 if __name__ == "__main__":
