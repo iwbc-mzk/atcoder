06be95c8473d2e6b4d8c6c6dcc21163167e33449
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-14 10:42:46 +0900

    typical90_007

--- typical90/007/main.py
+++ typical90/007/main.py
@@ -1,5 +1,26 @@
+import math
+import bisect
+
+
 def main():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    Q = int(input())
+    B = [int(input()) for _ in range(Q)]
+
+    A.sort()
+    for b in B:
+        i = bisect.bisect_left(A, b)
+        ans = math.inf
+
+        if i < N:
+            ans = min(ans, abs(A[i] - b))
+        if i + 1 < N:
+            ans = min(ans, abs(A[i + 1] - b))
+        if 0 <= i - 1 < N:
+            ans = min(ans, abs(A[i - 1] - b))
+
+        print(ans)
 
 
 if __name__ == "__main__":
