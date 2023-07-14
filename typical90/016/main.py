6bf03b0d12e39d57977ddd0d6b4cea5fd07bd37f
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-14 22:57:43 +0900

    typical90_016

--- typical90/016/main.py
+++ typical90/016/main.py
@@ -1,5 +1,35 @@
 def main():
-    pass
+    N = int(input())
+    A, B, C = map(int, input().split())
+
+    coins = [A, B, C]
+    coins.sort(reverse=True)
+
+    ans = 10**4
+
+    i = N // coins[0]
+    if N % coins[0] == 0:
+        ans = min(ans, i)
+
+    while i >= 0:
+        i_remain = N - coins[0] * i
+
+        j = i_remain // coins[1]
+        if i_remain % coins[1] == 0:
+            ans = min(ans, i + j)
+
+        while j >= 0:
+            j_remain = i_remain - coins[1] * j
+
+            k = j_remain // coins[2]
+            if j_remain % coins[2] == 0:
+                ans = min(ans, i + j + k)
+
+            j -= 1
+
+        i -= 1
+
+    print(ans)
 
 
 if __name__ == "__main__":
