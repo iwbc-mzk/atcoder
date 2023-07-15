8de36774f948d0a268cfd8d2f15c51f3712b20c3
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-15 12:03:14 +0900

    typical90_018

--- typical90/018/main.py
+++ typical90/018/main.py
@@ -1,5 +1,23 @@
+from math import pi, cos, sin, atan2, sqrt
+
+
 def main():
-    pass
+    T = int(input())
+    L, X, Y = map(int, input().split())
+    Q = int(input())
+
+    for _ in range(Q):
+        E = int(input())
+
+        theta = 3 * pi / 2 - 2 * pi * E / T
+
+        y = L / 2 * cos(theta)
+        z = L / 2 * (1 + sin(theta))
+
+        xx = sqrt(X**2 + (Y - y) ** 2)
+
+        ans = atan2(z, xx) / (2 * pi) * 360
+        print(ans)
 
 
 if __name__ == "__main__":
