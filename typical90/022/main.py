ac0c1cd246541c938d8f8992373d7acc7a12179f
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-15 23:07:01 +0900

    typical90_022

--- typical90/022/main.py
+++ typical90/022/main.py
@@ -1,5 +1,17 @@
+from math import gcd
+
+
 def main():
-    pass
+    A, B, C = map(int, input().split())
+
+    m = gcd(A, B)
+    m = gcd(m, C)
+
+    ans = 0
+    for i in [A, B, C]:
+        ans += i // m - 1
+
+    print(ans)
 
 
 if __name__ == "__main__":
