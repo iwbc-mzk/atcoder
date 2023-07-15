4e52d7eea211146b2fb56b22a4c2a604317f5017
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-15 23:13:40 +0900

    typical90_024

--- typical90/024/main.py
+++ typical90/024/main.py
@@ -1,5 +1,16 @@
 def main():
-    pass
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    s = 0
+    for a, b in zip(A, B):
+        s += abs(a - b)
+
+    if s <= K and abs(s - K) % 2 == 0:
+        print("Yes")
+    else:
+        print("No")
 
 
 if __name__ == "__main__":
