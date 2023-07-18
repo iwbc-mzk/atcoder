199cc799916aae0c237ac2460bd47acc6645ecdc
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-18 13:11:13 +0900

    typical90_038

--- typical90/038/main.py
+++ typical90/038/main.py
@@ -1,5 +1,14 @@
+from math import gcd
+
+
 def main():
-    pass
+    A, B = map(int, input().split())
+
+    ans = A * B // gcd(A, B)
+    if ans > 10**18:
+        print("Large")
+    else:
+        print(ans)
 
 
 if __name__ == "__main__":
