ed3453e0964d3bac7f2764c53d821b2dba7fbdaa
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-18 18:43:37 +0900

    typical90_042

--- typical90/042/main.py
+++ typical90/042/main.py
@@ -1,5 +1,22 @@
 def main():
-    pass
+    K = int(input())
+
+    MOD = 10**9 + 7
+
+    if K % 9:
+        print(0)
+        return
+
+    dp = [0 for _ in range(K + 1)]
+    dp[0] = 1
+    for i in range(1, K + 1):
+        for j in range(1, 10):
+            if 0 <= i - j:
+                dp[i] += dp[i - j] % MOD
+
+            dp[i] %= MOD
+
+    print(dp[K])
 
 
 if __name__ == "__main__":
