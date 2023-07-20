107e7d23824c13c0e502c97b5cfe458cd6e1efbd
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-20 14:04:56 +0900

    typical90_046

--- typical90/046/main.py
+++ typical90/046/main.py
@@ -1,5 +1,29 @@
+from collections import defaultdict
+
+
 def main():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    C = list(map(int, input().split()))
+
+    ad = defaultdict(int)
+    bd = defaultdict(int)
+    cd = defaultdict(int)
+
+    for a, b, c in zip(A, B, C):
+        ad[a % 46] += 1
+        bd[b % 46] += 1
+        cd[c % 46] += 1
+
+    ans = 0
+    for ai, av in ad.items():
+        for bi, bv in bd.items():
+            for ci, cv in cd.items():
+                if (ai + bi + ci) % 46 == 0:
+                    ans += av * bv * cv
+
+    print(ans)
 
 
 if __name__ == "__main__":
