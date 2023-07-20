062259ec079a4e4ee6b43e4d1d693a341ec58309
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-20 13:42:10 +0900

    typical90_044

--- typical90/044/main.py
+++ typical90/044/main.py
@@ -1,5 +1,19 @@
 def main():
-    pass
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    s = 0
+    for _ in range(Q):
+        t, x, y = map(int, input().split())
+        x = (x - 1 - s + N) % N
+        y = (y - 1 - s + N) % N
+
+        if t == 1:
+            A[x], A[y] = A[y], A[x]
+        elif t == 2:
+            s += 1
+        else:
+            print(A[x])
 
 
 if __name__ == "__main__":
