58eab883cb8285392d97102bd6cdbdd617ef8415
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-23 00:24:24 +0900

    typical90_055

--- typical90/055/main.py
+++ typical90/055/main.py
@@ -1,5 +1,25 @@
+from itertools import combinations
+
+
 def main():
-    pass
+    N, P, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    ans = 0
+    for i in range(N):
+        ti = A[i] % P
+        for j in range(i + 1, N):
+            tj = ti * A[j] % P
+            for k in range(j + 1, N):
+                tk = tj * A[k] % P
+                for l in range(k + 1, N):
+                    tl = tk * A[l] % P
+                    for m in range(l + 1, N):
+                        t = tl * A[m] % P
+                        t %= P
+                        if t == Q:
+                            ans += 1
+    print(ans)
 
 
 if __name__ == "__main__":
