d9677d70b0991cd161376010d3a711b74bae7ba0
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-23 14:56:30 +0900

    typical90_061

--- typical90/061/main.py
+++ typical90/061/main.py
@@ -1,5 +1,20 @@
 def main():
-    pass
+    Q = int(input())
+
+    u = []
+    l = []
+    for _ in range(Q):
+        t, x = map(int, input().split())
+        if t == 1:
+            u.append(x)
+        elif t == 2:
+            l.append(x)
+        else:
+            if x <= len(u):
+                print(u[-x])
+            else:
+                x -= len(u)
+                print(l[x - 1])
 
 
 if __name__ == "__main__":
