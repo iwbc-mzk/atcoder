21d48ee4d3271d676ace49ec752b1c7b1759a226
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-24 15:38:30 +0900

    typical90_063-064

--- typical90/063/main.py
+++ typical90/063/main.py
@@ -1,5 +1,42 @@
+from collections import defaultdict
+
+
 def main():
-    pass
+    H, W = map(int, input().split())
+    P = [list(map(int, input().split())) for _ in range(H)]
+
+    ans = 0
+    for hs in range(1 << H):
+        h_num = 0
+        for k in range(H):
+            if hs >> k & 1:
+                h_num += 1
+
+        if hs == 6:
+            pass
+
+        count = defaultdict(int)
+        for w in range(W):
+            c_val = -1
+            ok = True
+            for k in range(H):
+                if not (hs & 1 << k):
+                    continue
+
+                if c_val == -1:
+                    c_val = P[k][w]
+                else:
+                    if c_val != P[k][w]:
+                        ok = False
+                        break
+
+            if ok and c_val != -1:
+                count[c_val] += 1
+        else:
+            for key, val in count.items():
+                ans = max(ans, val * h_num)
+
+    print(ans)
 
 
 if __name__ == "__main__":
--- typical90/064/main.py
+++ typical90/064/main.py
@@ -1,5 +1,28 @@
 def main():
-    pass
+    N, Q = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    B = []
+    E = 0
+    for i in range(N - 1):
+        B.append(A[i + 1] - A[i])
+        E += abs(B[-1])
+
+    for _ in range(Q):
+        L, R, V = map(int, input().split())
+        L, R = L - 1, R - 1
+
+        if 0 < L:
+            E -= abs(B[L - 1])
+            B[L - 1] += V
+            E += abs(B[L - 1])
+
+        if R < N - 1:
+            E -= abs(B[R])
+            B[R] -= V
+            E += abs(B[R])
+
+        print(E)
 
 
 if __name__ == "__main__":
