81267f180b18eb0213560fcf9a434a28e65988d0
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-25 16:14:21 +0900

    Update pow.py

--- .helper/pow.py
+++ .helper/pow.py
@@ -1,11 +1,12 @@
 def pow(x, n, mod=1):
     """
-    xのn乗を計算する。
+    繰り返し2乗法を利用してxのn乗を計算する。
+    計算量はlog2(n)
     pypy3(7.3.0)では組み込みのpow関数が実装されていないため暫定的に利用する
     """
     if n == 0:
         return 1
     elif n % 2:
-        return (x * (pow(x, n - 1, mod) % mod)) % mod
+        return (x * pow(x, n - 1, mod)) % mod
     else:
-        return ((pow(x, n // 2, mod) % mod) ** 2) % mod
+        return (pow(x, n // 2, mod) ** 2) % mod
