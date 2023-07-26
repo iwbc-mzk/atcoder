5294eb1c2687ee6611aabdc69e001e1aa7631f77
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-26 16:25:54 +0900

    typical90_072

--- typical90/072/main.py
+++ typical90/072/main.py
@@ -1,5 +1,48 @@
+from collections import defaultdict
+
+
 def main():
-    pass
+    H, W = map(int, input().split())
+    C = [input() for _ in range(H)]
+
+    G = defaultdict(set)
+    for h in range(H):
+        for w in range(W):
+            if C[h][w] == "#":
+                continue
+
+            for dh, dw in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                hh, ww = h + dh, w + dw
+                if 0 <= hh < H and 0 <= ww < W and C[hh][ww] == ".":
+                    G[(h, w)].add((hh, ww))
+
+    ans = -1
+    for h in range(H):
+        for w in range(W):
+            if C[h][w] == "#":
+                continue
+
+            start = (h, w)
+            visited = set()
+            visited.add(start)
+
+            def dfs(v, d):
+                nonlocal ans
+                for vv in G[v]:
+                    if vv == start and d + 1 >= 4:
+                        ans = max(ans, d + 1)
+                        break
+
+                    if vv not in visited:
+                        visited.add(vv)
+                        dfs(vv, d + 1)
+                        visited.remove(vv)
+
+                return -1
+
+            dfs(start, 0)
+
+    print(ans)
 
 
 if __name__ == "__main__":
