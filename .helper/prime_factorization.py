da9904d67c48be92b6f6724e15a9ee7d0d7755ee
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-26 16:55:53 +0900

    Create prime_factorization.py

---
+++ .helper/prime_factorization.py
@@ -0,0 +1,21 @@
+from collections import defaultdict
+import math
+
+
+# 計算量O(√N)
+def prime_factorization(n: int):
+    result = defaultdict(int)
+    for i in range(2, math.ceil(math.sqrt(n)) + 1):
+        # 割り切れるだけ割る
+        while n % i == 0:
+            n //= i
+            result[i] += 1
+
+        if n == 1:
+            break
+
+    # 最後に残った商が1より大きければ、これが√Nより大きい唯一の素因数になる
+    if n > 1:
+        result[n] = 1
+
+    return result
