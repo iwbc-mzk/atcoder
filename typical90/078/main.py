915cbc71a5da6fb87d7dec22c67c8149a8320dab
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-27 13:38:16 +0900

    typical90_078

--- typical90/078/main.py
+++ typical90/078/main.py
@@ -1,5 +1,25 @@
+from collections import defaultdict
+
+
 def main():
-    pass
+    N, M = map(int, input().split())
+
+    G = defaultdict(int)
+    for _ in range(M):
+        a, b = map(int, input().split())
+        if b < a:
+            G[a] += 1
+        elif a < b:
+            G[b] += 1
+        else:
+            ...
+
+    ans = 0
+    for v in G.values():
+        if v == 1:
+            ans += 1
+
+    print(ans)
 
 
 if __name__ == "__main__":
