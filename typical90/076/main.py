42faf4aef6eb3fc0818d4aef011536c832c14aea
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-27 13:31:58 +0900

    typical90_076

--- typical90/076/main.py
+++ typical90/076/main.py
@@ -1,5 +1,26 @@
 def main():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    S = sum(A)
+    AA = A * 2
+
+    left, right = 0, 0
+    s = 0
+    while left < N:
+        while s < S / 10:
+            s += AA[right]
+            right += 1
+
+        if s == S / 10:
+            print("Yes")
+            return
+        else:
+            s -= AA[left]
+            left += 1
+    else:
+        print("No")
+        return
 
 
 if __name__ == "__main__":
