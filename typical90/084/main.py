21bd8a41f28305968930a13391ef584269d72510
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-28 16:06:48 +0900

    typical90_084

--- typical90/084/main.py
+++ typical90/084/main.py
@@ -1,4 +1,34 @@
+from typing import Iterable
+
+
+# ランレングス圧縮
+def run_length_encoding(s: Iterable):
+    pre = s[0]
+    cnt = 0
+    result = []
+    for si in s:
+        if si == pre:
+            cnt += 1
+        else:
+            result.append((pre, cnt))
+            pre = si
+            cnt = 1
+    else:
+        result.append((pre, cnt))
+
+    return result
+
+
 def main():
+    N = int(input())
+    S = input()
+
+    s = run_length_encoding(S)
+    ans = (N + 1) * (N) // 2
+    for _, c in s:
+        ans -= (c + 1) * c // 2
+
+    print(ans)
     pass
 
 
