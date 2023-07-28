8d4c7c09611ff932201f4bfd9f008cb6f5eb3771
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-28 16:36:16 +0900

    typical90_085

--- typical90/085/main.py
+++ typical90/085/main.py
@@ -1,5 +1,33 @@
+def enumerate_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n // i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+
 def main():
-    pass
+    K = int(input())
+
+    ed = enumerate_divisors(K)
+
+    ans = 0
+    for ai in range(len(ed)):
+        a = ed[ai]
+
+        for bi in range(ai, len(ed)):
+            b = ed[bi]
+
+            if a * b > K or K % (a * b) or b > K // (a * b):
+                continue
+
+            ans += 1
+
+    print(ans)
 
 
 if __name__ == "__main__":
