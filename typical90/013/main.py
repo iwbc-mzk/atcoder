1fda5d61065e9de39c70387a041e6c757939df77
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-07-31 15:53:19 +0900

    typical90_013

--- typical90/013/main.py
+++ typical90/013/main.py
@@ -1,5 +1,47 @@
+from collections import defaultdict, deque
+from math import inf
+
+
+def bfs_dist(s, G, D, N):
+    q = deque()
+    q.append((s, 0))
+    dis = [inf for _ in range(N)]
+    dis[s - 1] = 0
+    visited = [inf for _ in range(N)]
+    while q:
+        v, d = q.popleft()
+        if visited[v - 1] <= d:
+            continue
+        else:
+            visited[v - 1] = d
+
+        for vv in G[v]:
+            dd = d + D[v][vv]
+            if dd < dis[vv - 1]:
+                dis[vv - 1] = dd
+                q.append((vv, dd))
+
+    return dis
+
+
 def main():
-    pass
+    N, M = map(int, input().split())
+
+    G = defaultdict(set)
+    D = defaultdict(lambda: defaultdict(int))
+
+    for _ in range(M):
+        A, B, C = map(int, input().split())
+        G[A].add(B)
+        G[B].add(A)
+        D[A][B] = C
+        D[B][A] = C
+
+    dist1 = bfs_dist(1, G, D, N)
+    distN = bfs_dist(N, G, D, N)
+
+    for i in range(1, N + 1):
+        print(dist1[i - 1] + distN[i - 1])
 
 
 if __name__ == "__main__":
