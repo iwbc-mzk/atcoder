8f54684750bd5cf4368244e192169c56a88ec366
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-08-03 22:16:02 +0900

    typical90_030

--- typical90/030/main.py
+++ typical90/030/main.py
@@ -1,5 +1,20 @@
 def main():
-    pass
+    N, K = map(int, input().split())
+
+    c = [0 for _ in range(N + 1)]
+    for i in range(2, N + 1):
+        if c[i] != 0:
+            continue
+
+        for j in range(i, N + 1, i):
+            c[j] += 1
+
+    ans = 0
+    for ci in c:
+        if ci >= K:
+            ans += 1
+
+    print(ans)
 
 
 if __name__ == "__main__":
