d2d430c416e1550daf708eb6431079a7ccad325b
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-08-06 01:46:18 +0900

    abc297_e

--- abc/abc297/e/main.py
+++ abc/abc297/e/main.py
@@ -1,24 +1,25 @@
-import sys
-sys.setrecursionlimit(10 ** 8)
+import heapq
 
-def rec(i, A, price, ptn: set, limit):
-    if len(ptn) > limit:
-        return
-    
-    rec(i, A, price + A[i], ptn, limit)
-    ptn.add(price + A[i])
-
-    rec(i + 1, A, price, ptn, limit)
 
 def main():
     N, K = map(int, input().split())
     A = list(map(int, input().split()))
     A.sort()
 
-    ptn = set()
-    rec(0, A, 0, ptn, K)
-    print(list(ptn).sort()[K-1])
-    
+    ans = [-1] * (K + 1)
+    Q = []
+    heapq.heappush(Q, 0)
+    for i in range(K + 1):
+        v = heapq.heappop(Q)
+        if i >= 1 and ans[i - 1] == v:
+            while v == ans[i - 1]:
+                v = heapq.heappop(Q)
+        ans[i] = v
+
+        for j in range(N):
+            heapq.heappush(Q, v + A[j])
+
+    print(ans[K])
 
 
 if __name__ == "__main__":
