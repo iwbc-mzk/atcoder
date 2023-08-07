dda16e41ae7a78475d68f33ea9ad88a5b43d3d8e
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-08-07 15:16:23 +0900

    Update segment_tree.py

--- .helper/segment_tree.py
+++ .helper/segment_tree.py
@@ -2,51 +2,51 @@ from typing import List, Union, TypeVar, Optional, Callable
 from math import gcd
 
 T = TypeVar("T")
-Num = Union[int, float]
 
 
 # 参考: https://algo-logic.info/segment-tree/
 class SegTree:
     def __init__(
         self,
-        init_list: List[Num],
+        init: Union[List[T], int],
         type_: str = "min",
-        segfunc: Optional[Callable[[Num, Num], Num]] = None,
-        init_val: Optional[Num] = None,
+        segfunc: Optional[Callable[[T, T], T]] = None,
+        init_val: Optional[T] = None,
     ) -> None:
         """
         初期化処理
 
         Args:
-            init_list (List[Num]): セグメント木として管理するリスト
+            init_list (List[T]): セグメント木として管理するリスト
             type_ (str, optional): セグメント木のタイプ
                                    利用可能なのは"min", "max", "sum", "product", "gcd", "original"
                                    デフォルトは"min"
                                    "original"指定時はsegfuncとinit_valの指定も必要
-            segfunc (Optional[Callable[[Num, Num], Num]], optional): 各区間に行いたい処理
+            segfunc (Optional[Callable[[T, T], T]], optional): 各区間に行いたい処理
                                                                      type_で"original"指定時のみ有効
-            init_val (Optional[Num], optional): 各区間の初期値
+            init_val (Optional[T], optional): 各区間の初期値
                                                 type_で"original"指定時のみ有効
 
         Raises:
             ValueError: 無効なtype_を指定時
         """
-        self._n = 1 << (len(init_list) - 1).bit_length()
+        n: int = len(init) - 1 if type(init) == list else init - 1
+        self._n = 1 << n.bit_length()
 
         if type_ == "min":
-            self._segfunc = lambda x, y: min(x, y)
-            self._init_val = float("inf")
+            self._segfunc = min
+            self._init_val = 10**18
         elif type_ == "max":
-            self._segfunc = lambda x, y: max(x, y)
-            self._init_val = -float("inf")
+            self._segfunc = max
+            self._init_val = -(10**18)
         elif type_ == "sum":
-            self._segfunc = lambda x, y: x + y
+            self._segfunc = sum
             self._init_val = 0
         elif type_ == "product":
             self._segfunc = lambda x, y: x * y
             self._init_val = 1
         elif type_ == "gcd":
-            self._segfunc = lambda x, y: gcd(x, y)
+            self._segfunc = gcd
             self._init_val = 0
         elif type_ == "original":
             self._segfunc = segfunc
@@ -54,19 +54,32 @@ class SegTree:
         else:
             raise ValueError()
 
-        self._tree = [self._init_val] * 2 * self._n
+        self._tree = [self._init_val] * (2 * self._n - 1)
 
-        for i, v in enumerate(init_list):
-            self.update(i, v)
+        if type(init) == list:
+            # x個の初期値(葉)は self._n - 1 ~ x + self._n - 1 に格納される
+            # ノードは 0 ~ self._n - 2
+            self._tree[self._n - 1 : self._n - 1 + len(init)] = init
+            for i in range(self._n - 2, -1, -1):
+                self._update(i)
 
-    def update(self, i: int, x: int) -> None:
+    def __repr__(self) -> str:
+        l, r = 1, 2
+        res = []
+        while r <= 2 * self._n:
+            res.append(f"{self._tree[l:r]}")
+            l, r = r, r << 1
+
+        return "\n".join(res)
+
+    def update(self, i: int, x: T) -> None:
         """
         数列の値の更新を行う
         計算量 O(logN)
 
         Args:
             i (int): 更新したい数列の位置
-            x (int): 更新する値
+            x (T): 更新する値
         """
         # 数列のi番目の値はself._tree上ではn-1+i番目に格納されている
         i += self._n - 1
@@ -76,10 +89,13 @@ class SegTree:
         while i > 0:
             # 親ノードの位置
             i = (i - 1) // 2
-            # 親ノードの位置がiの時、子ノードは 2i+1, 2i+2 になる
-            self._tree[i] = self._segfunc(self._tree[i * 2 + 1], self._tree[i * 2 + 2])
+            self._update(i)
 
-    def query(self, a: int, b: int) -> int:
+    def _update(self, i: int) -> None:
+        # 親ノードの位置がiの時、子ノードは 2i+1, 2i+2 になる
+        self._tree[i] = self._segfunc(self._tree[i * 2 + 1], self._tree[i * 2 + 2])
+
+    def query(self, a: int, b: int) -> T:
         """
         区間 [a, b) で segfuncを実行した結果を返す
         計算量 O(logN)
@@ -89,29 +105,20 @@ class SegTree:
             b (int): 区間の最後のインデックス(区間に含まない)
 
         Returns:
-            int: 区間 [a, b) の最小値
+            T: 区間 [a, b) の最小値
         """
-        return self._sub_query(a, b, 0, 0, self._n)
+        l = max(a, 0) + self._n
+        r = min(b, self._n) + self._n
+        v = self._init_val
+        while l < r:
+            if l & 1:
+                v = self._segfunc(v, self._tree[l - 1])
+                l += 1
+            if r & 1:
+                r -= 1
+                v = self._segfunc(v, self._tree[r - 1])
 
-    def _sub_query(self, a: int, b: int, k: int, l: int, r: int) -> int:
-        """
-        _summary_
+            l >>= 1
+            r >>= 1
 
-        Args:
-            a (int): _description_
-            b (int): _description_
-            k (int): 現在見ているself._treeの位置
-            l (int): self._tree[k]が表している区間の開始位置(lを含む)
-            r (int): self._tree[k]が表している区間の終了位置(rを含まない)
-
-        Returns:
-            int: _description_
-        """
-        if r <= a or b <= l:
-            return self._init_val
-        elif a <= l and r <= b:
-            return self._tree[k]
-        else:
-            vl = self._sub_query(a, b, k * 2 + 1, l, (l + r) // 2)  # 左側子要素
-            vr = self._sub_query(a, b, k * 2 + 2, (l + r) // 2, r)  # 右側子要素
-            return self._segfunc(vl, vr)
+        return v
