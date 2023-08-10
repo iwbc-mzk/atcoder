b3ad6c75e7804a198a3b14c9ef2104544b2488fb
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-08-10 11:28:59 +0900

    abc241_a-b

--- abc/abc241/a/main.py
+++ abc/abc241/a/main.py
@@ -1,5 +1,10 @@
 def main():
-    pass
+    A = list(map(int, input().split()))
+    s = 0
+    for _ in range(3):
+        s = A[s]
+
+    print(s)
 
 
 if __name__ == "__main__":
--- abc/abc241/b/main.py
+++ abc/abc241/b/main.py
@@ -1,5 +1,23 @@
+from collections import defaultdict
+
+
 def main():
-    pass
+    N, M = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    C = defaultdict(int)
+    for a in A:
+        C[a] += 1
+
+    for b in B:
+        if b not in C or C[b] == 0:
+            print("No")
+            return
+        else:
+            C[b] -= 1
+
+    print("Yes")
 
 
 if __name__ == "__main__":
