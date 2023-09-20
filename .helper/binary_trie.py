89aa4bf92a7ec7dd4aec7e58f7aef3a3b152aa8c
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-09-20 20:45:33 +0900

    Update binary_trie.py

--- .helper/binary_trie.py
+++ .helper/binary_trie.py
@@ -1,8 +1,21 @@
 # 参考
 # https://kanpurin.hatenablog.com/entry/2021/09/05/163703
 # https://kanpurin.hatenablog.com/entry/2021/12/22/001854
+# https://ikatakos.com/pot/programming_algorithm/data_structure/trie
 class BinaryTrie:
-    def __init__(self, max_query=2 * 10**5, bitlen=30) -> None:
+    """
+    Binary Trie
+    Trie木の辺の値を文字でなく0/1に限定し、非負整数を2進数で管理する。
+    """
+
+    def __init__(self, max_query: int = 2 * 10**5, bitlen: int = 30) -> None:
+        """
+        初期化処理
+
+        Args:
+            max_query (int, optional): 管理する値の最大数. Defaults to 2*10**5.
+            bitlen (int, optional): 管理する非負数数値を表す最大2進数桁数. Defaults to 30.
+        """
         n = max_query * bitlen
         self._nodes = [-1] * (2 * n)
         self._cnt = [0] * n
@@ -12,7 +25,16 @@ class BinaryTrie:
     def size(self):
         return self._cnt[0]
 
-    def count(self, x):
+    def count(self, x: int) -> int:
+        """
+        xの個数を返す。
+
+        Args:
+            x (int): 非負整数
+
+        Returns:
+            int: xの個数
+        """
         pt = 0
         for i in range(self._bitlen - 1, -1, -1):
             y = (x >> i) & 1
@@ -22,7 +44,14 @@ class BinaryTrie:
 
         return self._cnt[pt]
 
-    def insert(self, x):
+    def insert(self, x: int) -> None:
+        """
+        非負整数xを挿入する。
+        管理する値の2進数での最大桁数がdの時、計算量は O(d)
+
+        Args:
+            x (int): 非負整数
+        """
         pt = 0
         for i in range(self._bitlen - 1, -1, -1):
             y = (x >> i) & 1
@@ -33,7 +62,15 @@ class BinaryTrie:
             pt = self._nodes[2 * pt + y]
         self._cnt[pt] += 1
 
-    def remove(self, x):
+    def remove(self, x: int) -> None:
+        """
+        非負整数xを取り除く。
+        xが存在しない場合は何もしない。
+        管理する値の2進数での最大桁数がdの時、計算量は O(d)
+
+        Args:
+            x (int): 非負整数
+        """
         if self.count(x) == 0:
             return
 
@@ -44,15 +81,25 @@ class BinaryTrie:
             pt = self._nodes[2 * pt + y]
         self._cnt[pt] -= 1
 
-    def kth_elm(self, x):
+    def kth_elm(self, k: int) -> int:
+        """
+        昇順k番目の値を返す。(1-indexed)
+        管理する値の2進数での最大桁数がdの時、計算量は O(d)
+
+        Args:
+            k (int): 非負整数
+
+        Returns:
+            int: k番目の値
+        """
         pt, ans = 0, 0
-        for i in range(self._bitlen - 1, -1, -1):
+        for _ in range(self._bitlen - 1, -1, -1):
             ans <<= 1
             if self._nodes[2 * pt] != -1 and self._cnt[self._nodes[2 * pt]] > 0:
-                if self._cnt[self._nodes[2 * pt]] >= x:
+                if self._cnt[self._nodes[2 * pt]] >= k:
                     pt = self._nodes[2 * pt]
                 else:
-                    x -= self._cnt[self._nodes[2 * pt]]
+                    k -= self._cnt[self._nodes[2 * pt]]
                     pt = self._nodes[2 * pt + 1]
                     ans += 1
             else:
@@ -60,7 +107,18 @@ class BinaryTrie:
                 ans += 1
         return ans
 
-    def lower_bound(self, x):
+    def lower_bound(self, x: int) -> int:
+        """
+        昇順に並べた時のx以上の最小要素の位置。(1-indexed)
+        x以上の要素がない場合はsize+1
+        管理する値の2進数での最大桁数がdの時、計算量は O(d)
+
+        Args:
+            x (int): 非負整数
+
+        Returns:
+            int: x以上の最小要素の位置
+        """
         pt, ans = 0, 1
         for i in range(self._bitlen - 1, -1, -1):
             if pt == -1:
