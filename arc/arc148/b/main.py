fdff50968aea29ec023311d9227d602af31cb0fe
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-09-20 20:17:22 +0900

    arc148_b

--- arc/arc148/b/main.py
+++ arc/arc148/b/main.py
@@ -1,5 +1,39 @@
 def main():
-    pass
+    N = int(input())
+    S = list(input())
+
+    l, r_list = None, []
+    t = 0
+    for i, s in enumerate(S):
+        if s == "p" and l is None:
+            l = i
+
+        if s == "p":
+            t += 1
+        else:
+            if t:
+                r_list.append(i - 1)
+            t = 0
+    else:
+        if S[-1] == "p":
+            r_list.append(N - 1)
+
+    if l is not None and r_list:
+        ans = []
+        for ri in r_list:
+            SS = S[:l] + list(reversed(S[l : ri + 1])) + S[ri + 1 :]
+            t = []
+            for i, s in enumerate(SS):
+                if l <= i <= ri:
+                    s = "p" if s == "d" else "d"
+                t.append(s)
+
+            ans.append("".join(t))
+        ans.sort()
+
+        print(ans[0])
+    else:
+        print("".join(S))
 
 
 if __name__ == "__main__":
