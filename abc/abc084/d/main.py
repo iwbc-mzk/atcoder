6420382e94d329718bb4265399247ee441b2487f
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-09-22 20:15:02 +0900

    abc084_d

--- abc/abc084/d/main.py
+++ abc/abc084/d/main.py
@@ -1,10 +1,39 @@
 import sys
+import bisect
 
 sys.setrecursionlimit(10**9)
 
 
+def enumerate_divisors(n):
+    lower_divisors, upper_divisors = [], []
+    i = 1
+    while i * i <= n:
+        if n % i == 0:
+            lower_divisors.append(i)
+            if i != n // i:
+                upper_divisors.append(n // i)
+        i += 1
+    return lower_divisors + upper_divisors[::-1]
+
+
 def main():
-    pass
+    Q = int(input())
+
+    primes = []
+    for i in range(1, 10**5 + 1, 2):
+        a = enumerate_divisors(i)
+        if len(a) == 2:
+            ii = (i + 1) // 2
+            b = enumerate_divisors(ii)
+            if len(b) == 2:
+                primes.append(i)
+
+    for _ in range(Q):
+        l, r = map(int, input().split())
+
+        ll = bisect.bisect_left(primes, l)
+        rr = bisect.bisect_right(primes, r)
+        print(rr - ll)
 
 
 if __name__ == "__main__":
