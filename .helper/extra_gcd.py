903c397f69a72dc8bc320e7b4898afb9a742f0d0
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-09-26 15:58:40 +0900

    Update extra_gcd.py

--- .helper/extra_gcd.py
+++ .helper/extra_gcd.py
@@ -1,4 +1,5 @@
 # ax + by = gcd(a, b) = d となる d, x, yを返す
+# mod P でのkの逆元計算はextGCD(P, k)のyになる
 def extGCD(a: int, b: int) -> tuple[int, int, int]:
     if b == 0:
         return (a, 1, 0)
@@ -9,3 +10,8 @@ def extGCD(a: int, b: int) -> tuple[int, int, int]:
     y -= q * x
 
     return (d, x, y)
+
+
+if __name__ == "__main__":
+    # mod 61 での25の逆元は 22
+    print(extGCD(61, 25))  # 22
