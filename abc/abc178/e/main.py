0d46b514dadd83a947b119ede14174e57615f727
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-10-18 16:18:01 +0900

    abc178_e

--- abc/abc178/e/main.py
+++ abc/abc178/e/main.py
@@ -1,5 +1,21 @@
 def main():
-    pass
+    N = int(input())
+    XY = [tuple(map(int, input().split())) for _ in range(N)]
+
+    max_x, min_x, max_y, min_y = (
+        -float("inf"),
+        float("inf"),
+        -float("inf"),
+        float("inf"),
+    )
+    for x, y in XY:
+        xx, yy = x + y, x - y
+        max_x = max(max_x, xx)
+        min_x = min(min_x, xx)
+        max_y = max(max_y, yy)
+        min_y = min(min_y, yy)
+
+    print(max(max_x - min_x, max_y - min_y))
 
 
 if __name__ == "__main__":
