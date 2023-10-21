8628e6894256a08bf18c67e597952dc1be8d1cee
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-10-21 23:03:09 +0900

    abc325_d

--- abc325/d/main.py
+++ abc325/d/main.py
@@ -9,10 +9,26 @@ def main():
     N = int(input())
     TD = [tuple(map(int, input().split())) for _ in range(N)]
     TD = [(t, t + d) for t, d in TD]
-    TD.sort(key=lambda x: (x[1], x[0]))
+    TD.sort()
+    TD = deque(TD)
 
     ans = 0
     now = 0
+    pq = []
+    for _ in range(N):
+        if not pq and TD:
+            now = TD[0][0]
+
+        while TD and TD[0][0] == now:
+            t, td = TD.popleft()
+            heapq.heappush(pq, (td, t))
+
+        while pq:
+            td, t = heapq.heappop(pq)
+            if t <= now <= td:
+                ans += 1
+                now += 1
+                break
 
     print(ans)
 
