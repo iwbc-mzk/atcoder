aee2da2b01a096e596aedee3b0b571b27ab65d08
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-03 18:22:29 +0900

    abc307_e

--- abc/abc307/e/main.py
+++ abc/abc307/e/main.py
@@ -1,5 +1,19 @@
 def main():
-    pass
+    N, M = map(int, input().split())
+    MOD = 998244353
+
+    dp = [[0, 0] for _ in range(N + 1)]
+    for n in range(1, N + 1):
+        if n == 1:
+            dp[n][1] = M
+        else:
+            dp[n][0] += dp[n - 1][0] * (M - 2) + dp[n - 1][1] * (M - 1)
+            dp[n][1] += dp[n - 1][0]
+
+        dp[n][0] %= MOD
+        dp[n][1] %= MOD
+
+    print(dp[N][0])
 
 
 if __name__ == "__main__":
