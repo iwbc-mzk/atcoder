ffdfe8e35e270c6db382645cb44bbf49d8338276
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-04 10:28:06 +0900

    review typical90_030

---
+++ typical90/030/review.py
@@ -0,0 +1,48 @@
+from typing import List
+
+
+# 参考:
+# https://ikatakos.com/pot/programming_algorithm/number_theory/prime_judge
+# https://manabitimes.jp/math/992
+# 計算量はO(NloglogN)
+def sieve_of_eratosthenes(n: int, return_flg: bool = False) -> List[int]:
+    # 偶数は2以外は素数でないので探索候補から外す
+    is_prime = [i if i % 2 else 0 for i in range(n + 1)]
+    is_prime[0], is_prime[1], is_prime[2] = 0, 0, 2
+
+    for p in range(3, int(n**0.5) + 1, 2):
+        if is_prime[p]:
+            for q in range(p**2, n + 1, 2 * p):
+                is_prime[q] = 0
+
+    if return_flg:
+        return is_prime
+    else:
+        primes = set(is_prime)
+        primes.remove(0)
+
+        return list(primes)
+
+
+def main():
+    N, K = map(int, input().split())
+
+    A = [0] * (N + 1)
+    primes = sieve_of_eratosthenes(N)
+    for p in primes:
+        A[p] += 1
+        np = p * 2
+        while np <= N:
+            A[np] += 1
+            np += p
+
+    ans = 0
+    for a in A:
+        if a >= K:
+            ans += 1
+
+    print(ans)
+
+
+if __name__ == "__main__":
+    main()
