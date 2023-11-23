3963824ea073b54f44ce5c601ff0bf4c33753e40
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-23 16:53:52 +0900

    abc273_d

--- abc/abc273/d/main.py
+++ abc/abc273/d/main.py
@@ -1,39 +1,65 @@
-def move(rs: int, cs: int, d: str, l: int, walls: set, H: int, W: int):
-    pos_r, pos_c = rs, cs
-    for _ in range(l):
-        if d == "L" and (pos_r, pos_c - 1) not in walls and pos_c - 1 > 0:
-            pos_c -= 1
-            continue
+from collections import defaultdict
+import bisect
 
-        if d == "R" and (pos_r, pos_c + 1) not in walls and pos_c + 1 <= W:
-            pos_c += 1
-            continue
-
-        if d == "U" and (pos_r - 1, pos_c) not in walls and pos_r - 1 > 0:
-            pos_r -= 1
-            continue
-
-        if d == "D" and (pos_r + 1, pos_c) not in walls and pos_r + 1 <= H:
-            pos_r += 1
-            continue
-
-        break
-
-    return pos_r, pos_c
 
 def main():
-    H, W, rs, cs = map(int, input().split())
+    H, W, RS, CS = map(int, input().split())
     N = int(input())
-    walls = {tuple(map(int, input().split())) for _ in range(N)}
-    
+    WALLS = [tuple(map(int, input().split())) for _ in range(N)]
     Q = int(input())
-    pos = (rs, cs)
+
+    RW = defaultdict(list)
+    for r, c in sorted(WALLS):
+        RW[r].append(c)
+
+    CW = defaultdict(list)
+    for r, c in sorted(WALLS, key=lambda x: (x[1], x[0])):
+        CW[c].append(r)
+
+    r, c = RS, CS
     for _ in range(Q):
         d, l = input().split()
         l = int(l)
 
-        pos = move(*pos, d, l, walls, H, W)
-        print(*pos)
+        if d == "U":
+            if c in CW:
+                i = bisect.bisect_left(CW[c], r)
+                if i == 0:
+                    r = max(1, r - l)
+                else:
+                    r = max(CW[c][i - 1] + 1, r - l)
+            else:
+                r = max(1, r - l)
+        elif d == "D":
+            if c in CW:
+                i = bisect.bisect_left(CW[c], r)
+                if i == len(CW[c]):
+                    r = min(H, r + l)
+                else:
+                    r = min(CW[c][i] - 1, r + l)
+            else:
+                r = min(H, r + l)
+        elif d == "L":
+            if r in RW:
+                i = bisect.bisect_left(RW[r], c)
+                if i == 0:
+                    c = max(1, c - l)
+                else:
+                    c = max(RW[r][i - 1] + 1, c - l)
+            else:
+                c = max(1, c - l)
+        elif d == "R":
+            if r in RW:
+                i = bisect.bisect_left(RW[r], c)
+                if i == len(RW[r]):
+                    c = min(W, c + l)
+                else:
+                    c = min(RW[r][i] - 1, c + l)
+            else:
+                c = min(W, c + l)
+
+        print(r, c)
+
 
 if __name__ == "__main__":
     main()
