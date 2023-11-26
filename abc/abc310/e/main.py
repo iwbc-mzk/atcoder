ea7970f9bedcf5369e44d3f42c86b11776a85cc2
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-26 21:18:35 +0900

    abc310_e

--- abc/abc310/e/main.py
+++ abc/abc310/e/main.py
@@ -1,28 +1,23 @@
+def NAND(x, y):
+    return 0 if x == y == 1 else 1
+
+
 def main():
     N = int(input())
     S = input()
 
-
-    SS = []
-    for i, s in enumerate(S):
-        if i == 0:
-            SS.append(int(s))
-        else:
-            if SS[i - 1] == 1 and int(s) == 1:
-                SS.append(0)
-            else:
-                SS.append(1)
-
-    SSS = [0]
-    for i, s in enumerate(SS, 1):
-        SSS.append(SSS[i - 1] + s)
-
+    dp = [[0, 0] for _ in range(N + 1)]
+    for i in range(N):
+        s = int(S[i])
+        dp[i + 1][NAND(0, s)] += dp[i][0]
+        dp[i + 1][NAND(1, s)] += dp[i][1]
+        dp[i + 1][s] += 1
 
     ans = 0
-    for i in range(1, N + 1):
-        ans += SSS[N] - SSS[i - 1]
+    for _, v in dp:
+        ans += v
 
-    pass
+    print(ans)
 
 
 if __name__ == "__main__":
