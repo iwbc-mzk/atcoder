eae4a6a82e321007285f81af8af0c7807bbdf5b3
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-27 17:36:15 +0900

    Update mod_comb.py

--- .helper/mod_comb.py
+++ .helper/mod_comb.py
@@ -1,24 +1,24 @@
-def mod_comb(n: int, k: int, mod: int, memo: list=None) -> int:
+# 参考: https://drken1215.hatenablog.com/entry/2018/06/08/210000
+def mod_comb(n: int, k: int, mod: int, memo: list = None) -> int:
     """
     nCkの組み合わせをmodの基で計算する。
+    計算量:
+        引数にてmemoを渡した場合 O(1)
+        渡さない場合は O(k)
 
     Args:
         n (int): _description_
         k (int): _description_
         mod (int): _description_
-        memo (list, optional): 1-indexedでの階乗メモ
+        memo (list, optional): 1-indexedでの階乗メモ(1~nまで)
 
     Returns:
         int: 計算結果
     """
-    if not memo:
-        memo = [1]
-        for i in range(n + 1):
-            memo.append((memo[i - 1] * i) % mod)
 
     def extGCD(a: int, b: int) -> tuple[int, int]:
         if b == 0:
-            return (a, 1, 0)
+            return (1, 0)
 
         q, r = a // b, a % b
         y, x = extGCD(b, r)
@@ -26,4 +26,19 @@ def mod_comb(n: int, k: int, mod: int, memo: list=None) -> int:
 
         return (x, y)
 
-    return memo[n] * extGCD(memo[n - k], mod)[0] * extGCD(memo[k], mod)[0]
+    if memo:
+        return memo[n] * extGCD(memo[n - k], mod)[0] * extGCD(memo[k], mod)[0]
+    else:
+        k = min(k, n - k)
+
+        top = 1
+        for v in range(n - k + 1, n + 1):
+            top = (top * v) % mod
+
+        bottom = 1
+        for v in range(1, k + 1):
+            bottom = (bottom * v) % mod
+
+        bottom_inv = extGCD(bottom, mod)[0]
+
+        return (top * bottom_inv) % mod
