e4f3772ad023e009d91c55f2f6e520294b0186b5
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-27 16:27:17 +0900

    arc149_b

--- arc/arc149/b/main.py
+++ arc/arc149/b/main.py
@@ -1,10 +1,31 @@
 import sys
+import bisect
 
 sys.setrecursionlimit(10**9)
 
 
 def main():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    AB = [(a, b) for a, b in zip(A, B)]
+    AB.sort()
+
+    B = [b for _, b in AB]
+
+    INF = float("inf")
+    dp = [INF for _ in range(N)]
+    for b in B:
+        i = bisect.bisect_right(dp, b)
+        dp[i] = b
+
+    lis_b = 0
+    for v in dp:
+        if v < INF:
+            lis_b += 1
+
+    print(N + lis_b)
 
 
 if __name__ == "__main__":
