0280f1c1bf415b61f31af33c3c4bb234e2357993
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-11-30 21:36:44 +0900

    abc251_e

--- abc/abc251/e/main.py
+++ abc/abc251/e/main.py
@@ -1,5 +1,26 @@
 def main():
-    pass
+    N = int(input())
+    A = list(map(int, input().split()))
+
+    ans = float("inf")
+
+    dp1 = [[0, 0] for _ in range(N + 1)]
+    dp1[1] = [float("inf"), A[0]]
+    for n in range(2, N + 1):
+        dp1[n][0] = dp1[n - 1][1]
+        dp1[n][1] = min(dp1[n - 1]) + A[n - 1]
+
+    ans = min(ans, min(dp1[N]))
+
+    dp2 = [[0, 0] for _ in range(N + 1)]
+    dp2[1] = [0, float("inf")]
+    for n in range(2, N + 1):
+        dp2[n][0] = dp2[n - 1][1]
+        dp2[n][1] = min(dp2[n - 1]) + A[n - 1]
+
+    ans = min(ans, dp2[N][1])
+
+    print(ans)
 
 
 if __name__ == "__main__":
