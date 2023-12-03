b3296489eb0f82b6588c8bddf76e3273ded858d9
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-12-03 16:41:40 +0900

    abc247_e

--- abc/abc247/e/main.py
+++ abc/abc247/e/main.py
@@ -1,5 +1,42 @@
+def calc(L, X, Y):
+    res = 0
+    l, r = 0, 0
+    cx, cy = 0, 0
+    while l < len(L):
+        while r < len(L) and not (cx != 0 and cy != 0):
+            if L[r] == X:
+                cx += 1
+            if L[r] == Y:
+                cy += 1
+            r += 1
+
+        if cx > 0 and cy > 0:
+            res += len(L) - (r - 1)
+        if L[l] == X:
+            cx -= 1
+        if L[l] == Y:
+            cy -= 1
+        l += 1
+
+    return res
+
+
 def main():
-    pass
+    N, X, Y = map(int, input().split())
+    A = list(map(int, input().split()))
+
+    AA = []
+    ans = 0
+    for a in A:
+        if Y <= a <= X:
+            AA.append(a)
+        elif AA:
+            ans += calc(AA, X, Y)
+            AA = []
+    else:
+        ans += calc(AA, X, Y)
+
+    print(ans)
 
 
 if __name__ == "__main__":
