5420b221564e3a57b1bfd4e83d63fabab5a6a71b
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-12-04 15:00:32 +0900

    Update multiset.py

--- .helper/multiset.py
+++ .helper/multiset.py
@@ -9,12 +9,15 @@ class Multiset:
     def __init__(self, init_val: Iterable[T] = []) -> None:
         self._add_list = list(init_val)
         heapq.heapify(self._add_list)
+        self._add_inv_list = [-v for v in init_val]
+        heapq.heapify(self._add_inv_list)
 
         self._add_cnt: dict[T, int] = defaultdict(int)
         for a in self._add_list:
             self._add_cnt[a] += 1
 
         self._del_list: list[T] = []
+        self._del_inv_list: list[T] = []
 
     def __len__(self) -> int:
         return self.size()
@@ -23,12 +26,31 @@ class Multiset:
         return self.count(value) > 0
 
     def add(self, value: T) -> None:
+        """
+        要素追加
+        計算量: O(log N)
+
+        Args:
+            value (T): _description_
+        """
         heapq.heappush(self._add_list, value)
+        heapq.heappush(self._add_inv_list, -value)
         self._add_cnt[value] += 1
 
     def remove(self, value: T) -> None:
+        """
+        要素削除
+        計算量: O(log N)
+
+        Args:
+            value (T): _description_
+
+        Raises:
+            ValueError: _description_
+        """
         if self._add_cnt[value] > 0:
             heapq.heappush(self._del_list, value)
+            heapq.heappush(self._del_inv_list, -value)
             self._add_cnt[value] -= 1
         else:
             raise ValueError(f"[{value}] does not exist.")
@@ -39,6 +61,13 @@ class Multiset:
             heapq.heappop(self._del_list)
 
         return self._add_list[0] if self.size() else None
+
+    def largest(self) -> Optional[T]:
+        while self._del_inv_list and self._add_inv_list[0] == self._del_inv_list[0]:
+            heapq.heappop(self._add_inv_list)
+            heapq.heappop(self._del_inv_list)
+
+        return -self._add_inv_list[0] if self.size() else None
 
     def size(self) -> int:
         return len(self._add_list) - len(self._del_list)
