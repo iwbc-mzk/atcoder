cf40ebb0370b3fee43778c4db7b290cf982afca4
iwbc-mzk <46069337+iwbc-mzk@users.noreply.github.com>
2023-12-08 14:08:58 +0900

    abc266_e

--- abc/abc266/e/main.py
+++ abc/abc266/e/main.py
@@ -1,5 +1,13 @@
 def main():
-    pass
+    N = int(input())
+
+    dp = [[0] * 6 for _ in range(N + 1)]
+    for n in range(1, N + 1):
+        for v in range(6):
+            dp[n][v] = max(v + 1, sum(dp[n - 1]) / 6)
+
+    ans = sum(dp[N]) / 6
+    print(ans)
 
 
 if __name__ == "__main__":
